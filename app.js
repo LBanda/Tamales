f8da85830c65bcb8375b28554ce272cd06508916
LBanda <A01610329@itesm.mx>
2021-05-26 00:33:22 -0500

    conexion bd

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hola');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'db/tamalesn',
+   port: 3306
+});
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+});
+connection.end();
