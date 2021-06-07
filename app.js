ab01f4b42ce935e44a791e47b61f30000865fce5
LBanda <A01610329@itesm.mx>
2021-06-07 17:19:02 -0500

    vistas

--- app.js
+++ app.js
@@ -26,16 +26,40 @@ app.get("/", (req, res) => {
    res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/inicio", (req, res) => {
+   res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.get("/schedule", (req, res) => {
    //Pedir dias disponibles de la bd
    //Ordenamos por dia de la semana
    //Pasar la data al html
-   res.sendFile(path.join(__dirname, "views", "entregas.html")); // ./views/entregas.html
+   res.sendFile(path.join(__dirname, "views", "entregas.html")); 
 });
 
 app.get("/delivery", (req, res) => {
-   res.sendFile(path.join(__dirname, "views", "compra1.html")); // ./views/entregas.html
-})
+   res.sendFile(path.join(__dirname, "views", "compra1.html")); 
+});
+
+app.get("/delivery2", (req, res) => {
+   res.sendFile(path.join(__dirname, "views", "compra2.html")); 
+});
+
+app.get("/domicilio", (req, res) => {
+   res.sendFile(path.join(__dirname, "views", "compra-domicilio.html")); 
+});
+
+app.get("/sucursal", (req, res) => {
+   res.sendFile(path.join(__dirname, "views", "compra-sucursal.html")); 
+});
+
+app.get("/registro", (req, res) => {
+   res.sendFile(path.join(__dirname, "views", "registro.html")); 
+});
+
+app.get("/login", (req, res) => {
+   res.sendFile(path.join(__dirname, "views", "login.html")); 
+});
 
 app.set("port", 500);
 app.listen(app.get("port"), () => {
