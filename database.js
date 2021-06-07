ec9c29a824ecf73f60ff7f9a39332b3d538af194
Saulrs98 <62858470+Saulrs98@users.noreply.github.com>
2021-06-07 13:18:10 -0500

    bd

--- database.js
+++ database.js
@@ -1,19 +1,18 @@
-
 var mysql = require('mysql');
 var connection = mysql.createConnection({
    host: 'localhost',
-   user: 'root',
-   password: '',
    database: 'db/tamalesn',
-   port: 3000
+   user: 'root',
+   password: 'root',
+   port: 8889
 }); 
 
-connection.connect(function(error){
-   if(error){
-      throw error;
+connection.connect(function(err){
+   if(err){
+      console.log('Algo salio mal');
    }else{
       console.log('Conexion exitosa');
-   }
+   } 
 }); 
-
+ 
  
